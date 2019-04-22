e75281c8ccd03c58d7b7b1eccf1c4f0665fd9c5f
santoshcbit2002 <43035801+santoshcbit2002@users.noreply.github.com>
2019-04-22 11:30:51 +0100

    Add files via upload

--- nlu_model.py
+++ nlu_model.py
@@ -4,7 +4,11 @@ from rasa_nlu.model import Trainer
 from rasa_nlu.model import Metadata, Interpreter
 from rasa_nlu import config
 from rasa_nlu.components import ComponentBuilder
+import city_scrapperpy
 
+filename = './data/locationlist.txt'
+city_scrapperpy.scrapper_func(filename)
+ 
 builder = ComponentBuilder(use_cache=True)
 
 def train_nlu(data, config_file, model_dir):
@@ -15,8 +19,8 @@ def train_nlu(data, config_file, model_dir):
 	
 def run_nlu():
 	interpreter = Interpreter.load('./models/nlu/default/restaurantnlu', builder)
-	print(interpreter.parse("find some restaurants in mumbai"))
+	print(interpreter.parse("erode"))
 	
-if __name__ == '__main__':
+if __name__ == '__main__':	
 	train_nlu('./data/data.json', 'config_spacy.json', './models/nlu')
 	run_nlu()
