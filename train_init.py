ac27037f07a334d293ad08e12e20dd74621e761b
saman.tamkeen <saman.tamkeen@go-mmt.com>
2019-04-29 14:33:55 +0530

    train_init changes

--- train_init.py
+++ train_init.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 import logging
 
 from rasa_core.agent import Agent
+from rasa_core.policies.form_policy import FormPolicy
 from rasa_core.policies.keras_policy import KerasPolicy
 from rasa_core.policies.memoization import MemoizationPolicy
 
@@ -17,7 +18,7 @@ if __name__ == '__main__':
 	memoization_policy = MemoizationPolicy(max_history=4)
 	keras_policy = KerasPolicy(max_history=5, epochs=500)
 
-	agent = Agent('restaurant_domain.yml', policies=[memoization_policy, keras_policy])
+	agent = Agent('restaurant_domain.yml', policies=[memoization_policy, keras_policy, FormPolicy()])
 	training_data = agent.load_data(training_data_file, augmentation_factor = 50)
 	agent.train(training_data)
 	agent.persist(model_path)
